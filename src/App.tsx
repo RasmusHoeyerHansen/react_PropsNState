9bb2a2548a98a549c9536f5a68b3e6473614302a
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-08-25 20:54:28 +0200

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,38 @@
 import ExpenseItem, {ExpenseItemProp} from "./Components/ExpenseItem";
 
 function App() {
-    const today = new Date();
+	const today = new Date();
 
-    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	const date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
-    const itemProps: ExpenseItemProp[] = [
-        {
-            title: "bøh",
-            amount: 4,
-            day: today.getDate(),
-            year: today.getFullYear(),
-            month: today.getMonth()
-        },
-        {
-            title: "æv",
-            amount: 0,
-            day: today.getDate(),
-            year: today.getFullYear(),
-            month: today.getMonth()
-        },
-        {
-            title: "banan",
-            amount: 1,
-            day: today.getDate(),
-            year: today.getFullYear(),
-            month: today.getMonth()
-        }
-    ]
+	const expenses: ExpenseItemProp[] = [
+		{
+			title: "Toilet Paper",
+			amount: 94.12,
+			date: new Date(2020, 7, 14),
+		},
+		{title: "New TV", amount: 799.49, date: new Date(2021, 2, 12)},
+		{
+			title: "Car Insurance",
+			amount: 294.67,
+			date: new Date(2021, 2, 28),
+		},
+		{
+			title: "New Desk (Wooden)",
+			amount: 450,
+			date: new Date(2021, 5, 12),
+		},
+	];
 
-
-    return (
-        <div>
-            <h2>Let's get started!</h2>
-
-            <ExpenseItem  }/>
-        </div>
-    );
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<ExpenseItem
+				title={expenses[0].title}
+				date={expenses[0].date}
+				amount={expenses[0].amount}/>
+		</div>
+	);
 }
 
 export default App;
--- src/Components/ExpenseItem.tsx
+++ src/Components/ExpenseItem.tsx
@@ -1,33 +1,46 @@
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 
-export type ExpenseItemProp = {
-    title: string;
-    amount: number;
-} & ExpenseDateProps
-
-function ExpenseItem(props: ExpenseItemProp): JSX.Element {
-    return (
-        <div className="expense-item">
-            <div>DATE:   </div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-            </div>
-            <div className="expense-item__price">
-                {props.amount}
-            </div>
-        </div>
-    );
+export interface ExpenseItemProp {
+	title: string,
+	amount: number,
+	date: Date
 }
+
+
+const ExpenseItem = (props : ExpenseItemProp): JSX.Element => {
+	const day = props.date.getDate();
+	const month = props.date.getMonth();
+	const year = props.date.getFullYear();
+
+	return (
+		<div className="expense-item">
+
+			<ExpenseDate day={day} month={month} year={year} />
+
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+			</div>
+			<div className="expense-item__price">
+				{props.amount}
+			</div>
+		</div>
+	);
+};
 
 type ExpenseDateProps = {
-    day : number;
-    month :number;
-    year : number;
+	day: number;
+	month: number;
+	year: number;
 }
-function ExpenseDate (props: ExpenseDateProps) {
-    const today = new Date();
-    today.toLocaleString('da', {year: 'numeric', month: '2-digit', day: '2-digit'})
 
+function ExpenseDate(props: ExpenseDateProps) {
+	const today = new Date();
+	const dayString = today.toLocaleString("da", {
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit"
+	});
+	return <div>{dayString}</div>;
 
 }
 
