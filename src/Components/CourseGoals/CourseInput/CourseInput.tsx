c85634b68674ce8b5f55d1fc48aa8900bf519d6b
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-08-31 11:25:18 +0200

    dynamic styled compoinent

--- src/Components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/Components/CourseGoals/CourseInput/CourseInput.tsx
@@ -3,10 +3,47 @@ import React, {FormEvent, useState} from "react";
 import AddGoalButton     from "../../UI/Button/Button";
 import "./CourseInput.css";
 import {HTMLChangeEvent} from "../../../TypesAndClasses/HTMLChangeEvent";
+import styled            from "styled-components";
 
 interface ICourseInput {
 	onAddGoal: (enteredText: string) => void;
 }
+
+const FormControl = styled.div `
+
+  margin: 0.5rem 0;
+  
+
+   &label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  &input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input{
+    border-color: red;
+    background: mistyrose;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 
 const CourseInput = (props: ICourseInput) => {
@@ -32,11 +69,11 @@ const CourseInput = (props: ICourseInput) => {
 	};
 
 	return (<form onSubmit={formSubmitHandler}>
-		<div className={`form-control ${!isValid ? "invalid" : ""}`}>
+		<FormControl className={!isValid ? 'invalid' : '' }>
 			<input
 				type="text" onChange={goalInputChangeHandler}
 			/>
-		</div>
+		</FormControl>
 		<AddGoalButton type="submit">Add Goal</AddGoalButton>
 	</form>);
 };
--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,20 +1,34 @@
 import React, {
-  ButtonHTMLAttributes,
-  MouseEventHandler, PropsWithChildren
-} from "react";
+	ButtonHTMLAttributes, MouseEventHandler, PropsWithChildren
+}             from "react";
+import styled from "styled-components";
 
-import './Button.css';
 interface ICustomButton extends PropsWithChildren<ButtonHTMLAttributes<any>> {
-  type: 'submit' | 'reset' | 'button' | undefined,
-  onclick?: MouseEventHandler;
+	type: "submit" | "reset" | "button" | undefined,
+	onclick?: MouseEventHandler;
 }
 
-const Button = (props : ICustomButton) => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+//Using styled components
+const Button = styled.button<ICustomButton>`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+
+`;
 
 export default Button;
